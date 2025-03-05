53b8f692e1b2577c8ccdb3fab5c76e4907b994fd
Radcabna <radcabna@gmail.com>
2025-03-05 22:02:57 +0300

    Initial Commit

---
+++ Potawatomi Online/Potawatomi_OnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Potawatomi_OnlineApp.swift
+//  Potawatomi Online
+//
+//  Created by Алкександр Степанов on 05.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Potawatomi_OnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
